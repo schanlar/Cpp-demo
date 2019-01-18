fdbd871d3b81ac623d0f1d11aa6722623ea432b2
Savvas Chanlaridis <schanlaridis@gmail.com>
2019-01-18 12:52:33 +0100

    The header file of the class

---
+++ Classes/Contacts.h
@@ -0,0 +1,44 @@
+#ifndef CONTACTS_H
+#define CONTACTS_H
+
+#include <string>
+
+
+
+class Contacts {
+
+public:
+
+  // prototype Constructors
+  Contacts();
+  Contacts(std::string name, std::string lastName, std::string email, long phone);
+
+  // prototype Destructor
+  ~Contacts();
+
+  // prototype getters
+  std::string getName();
+  std::string getLastName();
+  std::string getEmail();
+  long getPhone();
+
+
+  //prototype setters
+  void setName(std::string newName);
+  void setLastName(std::string newLastName);
+  void setEmail(std::string newEmail);
+  void setPhone(long newPhone);
+
+
+private:
+  std::string theName;
+  std::string theLastName;
+  std::string theEmail;
+
+  long thePhone;
+
+
+};
+
+
+#endif // CONTACTS_H
